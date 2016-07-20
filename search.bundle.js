e1d014eebd270fda5a07ab916e318f4a35bd53cf
Galnykin Dmitriy <developer085@gmail.com>
2016-07-20 20:08:22 +0300

    NDP

---
+++ search.bundle.js
@@ -0,0 +1,5 @@
+webpackJsonp([2],[/*!*****************************!*\
+  !*** ./src/pages/search.js ***!
+  \*****************************/
+function(n,p){"use strict";app=window.app||{},console.warn("!!search!!")}]);
+//# sourceMappingURL=search.bundle.js.map
