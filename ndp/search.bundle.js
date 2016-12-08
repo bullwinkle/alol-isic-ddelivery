675c57f2405e11cc7ef02714efbe155b6c1140c2
Galnykin Dmitriy <developer085@gmail.com>
2016-12-08 18:48:25 +0530

    premium-gun

--- ndp/search.bundle.js
+++
@@ -1,5 +0,0 @@
-webpackJsonp([2],[/*!*****************************!*\
-  !*** ./src/pages/search.js ***!
-  \*****************************/
-function(n,p){"use strict";app=window.app||{},console.warn("!!search!!")}]);
-//# sourceMappingURL=search.bundle.js.map
